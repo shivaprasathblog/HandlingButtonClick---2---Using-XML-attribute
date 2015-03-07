fbbdc308bbda4edc71984be266407fea1d40def7
Shivaprasath <shiva.github@gmail.com>
2015-03-07 14:27:10 +0530

    HandlingButtonClick  -2 - Using XML attribute
    This piece of code used handle button click using XML attribute
    "onclick", we need to declare the method name in "onclick" and the same
    has to be used in the Activity(javacode) to handle click event.

---
+++ src/com/example/handlingbuttonclick/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.handlingbuttonclick;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity 
+{
+
+	Button b;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	
+	}
+	
+	
+	public void clickEvent(View v) 
+	{
+		Toast.makeText(MainActivity.this,"U have Clicked a Button", Toast.LENGTH_LONG).show();
+		
+		
+	}
+
+	
+	
+	
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
